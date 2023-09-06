36d56625cd1bf7df7f95ebc21c33e9e2632eb83f
JacquesIversen <jackiracki1@gmail.com>
2023-09-06 13:00:12 +0000

    react course done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,10 @@
 import css from './App.module.css';
-import ClassCounter from './components/ClassCounter';
-import Clock from './components/Clock';
-import Content from './components/Contens';
-import ControlledForm from './components/ControlledForm';
-import ControlledFormHooks from './components/ControlledFormHooks';
-import HooksCounter from './components/HooksCounter';
-import LifeCyclesCDM from './components/LifeCyclesCDM';
-import LifeCyclesCDU from './components/LifeCyclesCDU';
-import LifeCyclesCWU from './components/LifeCyclesCWU';
-import NavBarForm from './components/NavBarForm';
-import RenderingLists from './components/RenderingLists';
-import SearchBar from './components/SearchBar';
-import Sidebar from "./components/Sidebar";
-import Apps from './components/StatefulGreeting';
-import UseStateWithArrays from './components/UseStateWithArrays';
+import HTTPHooks from './components/HHTPHooks';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
-{/*       <Sidebar /> 
-      <Apps />
-      <NavBarForm />
-      <Clock />
-      <RenderingLists />
-      <LifeCyclesCDM />
-      <LifeCyclesCDU />
-      <LifeCyclesCWU />
-      <ControlledForm />
-      <SearchBar />
-      <HooksCounter />
-      <ClassCounter />
-      <ControlledFormHooks />
-      <Content /> */}
-      <UseStateWithArrays />
-
+      <HTTPHooks />
     </div>
   );
 }
